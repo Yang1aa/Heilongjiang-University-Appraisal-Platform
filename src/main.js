753aab36f5f166e964c4906e78b043189ab83837
杨柳岸 <88012771+Yang1aa@users.noreply.github.com>
2024-05-29 13:18:55 +0800

    更改ip

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: 杨柳岸 88012771+Yang1aa@users.noreply.github.com
  * @Date: 2023-12-20 22:14:47
  * @LastEditors: 杨柳岸 88012771+Yang1aa@users.noreply.github.com
- * @LastEditTime: 2024-01-11 21:48:26
+ * @LastEditTime: 2024-05-27 09:17:14
  * @FilePath: \imageupload-vue\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,7 +19,7 @@ import '@/styles/global.css'; // 引入全局样式
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = "http://116.63.8.192:8088" //后端服务器访问地址
+axios.defaults.baseURL = "http://116.63.15.173:8088" //后端服务器访问地址
 // axios.defaults.baseURL = "http://localhost:8088" //后端服务器访问地址
 
 //注册插件
